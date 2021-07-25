81c169ce880d7d91f36e6744c08239cc6d847e39
豆角 <52407673+DouJiao1122@users.noreply.github.com>
2021-07-25 17:18:44 +0800

    Add files via upload

---
+++ doujiao_web1.0/static/js/url.js
@@ -0,0 +1,19 @@
+var weChatWait=4;
+
+function qqqun(){
+  window.open("https://jq.qq.com/?_wv=1027&k=FRNW8mn4");
+}
+
+function qqemail(){
+  window.open("mailto:2569767455@qq.com")
+}
+
+function MinecraftPC(){
+    window.open("http://www.yutongdst.com:2020/fs/download/HMCL-3.3.172.exe")
+}
+
+function MinecraftPE(){
+    window.open("http://www.yutongdst.com:2020/fs/download/Minecraft.apk")
+}
+
+
